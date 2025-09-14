95bf43c9a82370bb4c38d2d37206d7ed81e0ab70
Nico Aroca <aroca.twelve@proton.me>
2025-09-14 19:59:38 -0400

    Update screenshot_selection_x11.sh

--- screenshot_selection_x11.sh
+++ screenshot_selection_x11.sh
@@ -2,22 +2,41 @@
 
 MAIN_DIR="$HOME/Pictures/Screenshots"
 
-# Get current year and month
 YEAR=$(date '+%Y')
-MONTH=$(date '+%B') # simpler than your case/switch
+MONTH=$(date '+%B')
 SAVE_DIR="$MAIN_DIR/$YEAR/$MONTH"
 mkdir -p "$SAVE_DIR"
 
-# Name format
 NAME="$(date '+%Y-%m-%d_%H-%M-%S').png"
 
-# Use Coreshot and save the image
-# coreshot --selection "$SAVE_DIR/$NAME"
+# Default mode = monitor
+MODE=${1:-monitor}
 
-# Launch coreshot in selection mode, unmanaged
-env SDL_VIDEO_X11_OVERRIDE_REDIRECT=1 \
-    SDL_VIDEO_X11_WMCLASS=Coreshot \
-    SDL_VIDEO_X11_NET_WM_BYPASS_COMPOSITOR=0 \
-    coreshot --selection "$SAVE_DIR/$NAME"
+if [ "$MODE" = "window" ]; then
+    # Capture active window
+    scrot -u "$SAVE_DIR/$NAME"
+    notify-send "📸 Screenshot (window) saved to $SAVE_DIR/$NAME"
+else
+    # Capture monitor containing active window
+    ACTIVE_WIN=$(xdotool getactivewindow)
+    GEOMETRY=$(xwininfo -id "$ACTIVE_WIN" | awk '/Absolute upper-left X|Absolute upper-left Y|Width|Height/ {print $NF}' | tr '\n' ' ')
+    read X Y W H <<< "$GEOMETRY"
 
-#notify-send "Screenshot saved to $SAVE_DIR/$NAME"
+    MONITOR=$(xrandr --current | awk '/ connected/ {print $1,$3}' | while read name geom; do
+        MONX=$(echo $geom | cut -d+ -f2)
+        MONY=$(echo $geom | cut -d+ -f3)
+        MONW=$(echo $geom | cut -d'x' -f1)
+        MONH=$(echo $geom | cut -d'x' -f2 | cut -d+ -f1)
+        if (( X >= MONX && X < MONX+MONW && Y >= MONY && Y < MONY+MONH )); then
+            echo "$MONX,$MONY,$MONW,$MONH"
+            break
+        fi
+    done)
+
+    IFS=, read MX MY MW MH <<< "$MONITOR"
+    scrot -a $MX,$MY,$MW,$MH "$SAVE_DIR/$NAME"
+    notify-send "🖥️ Screenshot (monitor) saved to $SAVE_DIR/$NAME"
+fi
+
+
+# Feels good to FINALLY update this LOL
